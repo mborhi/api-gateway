e1645e0c1986d9561a51c62e1694854868d6e3a2
Marcell Borhi <mborhi@iu.edu>
2022-08-20 11:13:56 -0400

    add spotify api types

---
+++ interfaces/index.ts
@@ -0,0 +1,31 @@
+// authentication
+export interface SpotifyToken {
+    "access_token": string,
+    "token_type": string,
+    "scope": string,
+    "expires_at": number,
+    "refresh_token": string
+}
+
+// API
+export interface ErrorObject {
+    "error": {
+        "status": number
+        "message": string
+    }
+}
+
+// retrieval
+export interface TrackData {
+    name: string
+    previewURL: string
+    trackURI: string
+    trackNum: number
+    trackAlbumImage: string
+}
+
+// playback
+export interface PlaybackStateData {
+    progress: number
+    availableActions: SpotifyApi.ActionsObject
+}
